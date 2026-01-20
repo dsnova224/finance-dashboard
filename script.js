e17b756898e062ee1e4a9de2262f7e371f841954
dsnova224 <83909108+dsnova224@users.noreply.github.com>
2026-01-20 13:42:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // CONFIGURATION - UPDATE THIS URL AFTER DEPLOYING CODE.GS
-const API_URL = "https://script.google.com/macros/s/AKfycbx7_KBIAeG0QfzrVwhNHnIlAJht3MhRlC2RuVwuGCrfsUTE-cfCIrlRMIDF9stnKl8w/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzzrA68exwxtGAWAyzEymROzhu88VTSG1_-VIcy4S2sWpeCuKOsPmhgaJRxCKGnSFMk/exec";
 const API_TOKEN = "omom@123OM";
 
 // Currency State
@@ -260,6 +260,7 @@ form.addEventListener('submit', async (e) => {
 function formatMoney(amount, symbol) {
     return symbol + ' ' + amount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 }
+
 
 
 
