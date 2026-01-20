0884f76fab17c472f2faba132d6a223d535209a1
dsnova224 <83909108+dsnova224@users.noreply.github.com>
2026-01-20 17:32:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // CONFIGURATION - UPDATE THIS URL AFTER DEPLOYING CODE.GS
-const API_URL = "https://script.google.com/macros/s/AKfycbwvmNVA_PD18Y9hYb3-MNEgnngARAU0_ho2f7vD3jvDjM-6NmFSjw5tXKpAv1KBWUTv/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxTu9cyLRFufc9Wzf60YNZWFAcwEFAcd0_WrhvHMqe5JWlpQWhxtoAwogL3wu3LEII/exec";
 const API_TOKEN = "omom@123OM";
 
 // Currency State
@@ -258,7 +258,7 @@ form.addEventListener('submit', async (e) => {
 });
 
 function formatMoney(amount, symbol) {
-    return symbol + ' ' + amount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    return symbol + ' ' + (amount || 0).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 }
 
 
